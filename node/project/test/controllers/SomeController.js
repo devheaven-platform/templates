d0da8d47b0b8705815b5234a399a944129513430
Mario <mario.olislaegers@gmail.com>
2019-03-12 11:37:04 +0100

    eslint updated to AirBnB

--- node/project/test/controllers/SomeController.js
+++ node/project/test/controllers/SomeController.js
@@ -1,10 +1,10 @@
-const chai = require('chai');
+const chai = require( "chai" );
 
 chai.should();
 
-describe('SomeController', () => {
-    it('should do something', async() => {
-        const string = 'name';
-        string.should.be.equal('name');
-    });
-});
+describe( "SomeController", () => {
+    it( "should do something", async () => {
+        const string = "name";
+        string.should.be.equal( "name" );
+    } );
+} );
