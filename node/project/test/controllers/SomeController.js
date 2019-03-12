27f9aad341943ec711d48a8fcb24c76324e25a8a
mario133710 <mario.olislaegers@gmail.com>
2019-03-12 11:38:08 +0100

    Add node template (#3)
    * Update golang template .gitignore
    
    * node template
    
    * ESLint & Testing configuration
    
    * eslint updated to AirBnB

---
+++ node/project/test/controllers/SomeController.js
@@ -0,0 +1,10 @@
+const chai = require( "chai" );
+
+chai.should();
+
+describe( "SomeController", () => {
+    it( "should do something", async () => {
+        const string = "name";
+        string.should.be.equal( "name" );
+    } );
+} );
