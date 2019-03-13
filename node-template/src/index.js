0c0df3d5f5071e83451473ebefc553cd82c2947c
tomdewildt <t.dewildt@student.fontys.nl>
2019-03-13 21:38:32 +0100

    Add base server & winston logger

---
+++ node-template/src/index.js
@@ -0,0 +1,46 @@
+require( "dotenv" ).config();
+const express = require( "express" );
+const bodyparser = require( "body-parser" );
+const cors = require( "cors" );
+const mongoose = require( "mongoose" );
+const logger = require( "./config/logger" );
+
+const app = express();
+app.disable( "x-powered-by" );
+
+// Server config
+const port = process.env.PORT;
+const host = process.env.HOSTNAME;
+const mongoURI = process.env.MONGO_URI;
+
+// Middleware
+app.use( bodyparser.json() );
+app.use( cors() );
+
+// Connect database
+mongoose
+    .connect( mongoURI, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false } )
+    .then( () => logger.info( "MongoDB connected" ) )
+    .catch( error => logger.error( error.stack ) );
+
+// Not found
+app.all( "*", ( req, res ) => res.status( 404 ).json( {
+    name: "NotFound",
+    message: "Resource Not Found",
+    status: 404,
+} ) );
+
+// Global error handler
+app.use( ( error, req, res ) => {
+    logger.error( "An unhandled exception occurred", error );
+    return res.status( 500 ).json( {
+        name: "InternalServerError",
+        message: "An error ocurred",
+        status: 500,
+    } );
+} );
+
+// Start service
+app.listen( port, () => {
+    logger.info( `Service: listening on http://${ host }:${ port }` );
+} );
