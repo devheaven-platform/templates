4b7f1756d088192c60120b48696adb6956bda09b
Tom de Wildt <32359630+tomdewildt@users.noreply.github.com>
2019-03-15 11:12:11 +0100

    Add dockerfile to asp template (#19)
    * Add python template
    
    * Update ASP.net template
    
    * Add error model to template
    
    * Add dockerfile & dockerignore
    
    * Add configuration getter to Startup.cs

---
+++ asp-template/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace AspTemplate
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (File.Exists(".env"))
+            {
+              DotNetEnv.Env.Load();
+            }
+
+            string host = System.Environment.GetEnvironmentVariable("ASP_HOST");
+            string port = System.Environment.GetEnvironmentVariable("ASP_PORT");
+
+            CreateWebHostBuilder(args, host, port).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args, string host, string port) => WebHost
+            .CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .UseUrls($"http://{host}:{port}");
+    }
+}
